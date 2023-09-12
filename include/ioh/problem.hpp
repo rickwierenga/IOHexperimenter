4dba375611f3629d1d002a6d8ccb4c70307c92e5
Dimitri Rusin <dimitri@habimm.com>
2023-09-12 11:09:32 +0200

    remove an include statement and sort the remaining ones

--- include/ioh/problem.hpp
+++ include/ioh/problem.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "problem/bbob.hpp"
-#include "problem/cec.hpp"
+#include "problem/bbob.hpp"
 #include "problem/dynamic_bin_val.hpp"
+#include "problem/pbo.hpp"
 #include "problem/pbo.hpp"
 #include "problem/problem.hpp"
 #include "problem/single.hpp"
